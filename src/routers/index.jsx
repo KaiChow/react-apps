b9cafb6ebabfca112ca35434a93635189771c702
KevinZhou <1065438834@qq.com>
2023-04-14 18:16:45 +0800

    测试提交

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -3,15 +3,26 @@ import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../views/ErrorPage";
 import Products from "../views/Products";
 import Root from "./root";
+import Dashboard from "../views/Dashboard";
+import Comments from "../views/Comments";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+
     children: [
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
       {
         path: "products",
         element: <Products />,
+      },
+      {
+        path: "comments",
+        element: <Comments />,
       },
     ],
   },
--- src/routers/root.jsx
+++ src/routers/root.jsx
@@ -1,9 +1,58 @@
+import { Card, List, Layout } from "antd";
 import React from "react";
 import { Outlet } from "react-router-dom";
+const { Header, Footer, Sider, Content } = Layout;
+
+const headerStyle = {
+  textAlign: "center",
+  color: "#fff",
+  height: 64,
+  paddingInline: 50,
+  lineHeight: "64px",
+  backgroundColor: "#7dbcea",
+};
+
+const contentStyle = {
+  textAlign: "center",
+  minHeight: 120,
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#108ee9",
+};
+
+const siderStyle = {
+  textAlign: "center",
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#3ba0e9",
+};
+
+const footerStyle = {
+  textAlign: "center",
+  color: "#fff",
+  backgroundColor: "#7dbcea",
+};
 export default function Root() {
+  const data = ["dashboard", "products", "comments"];
   return (
-    <>
-      <Outlet />
-    </>
+    <Layout>
+      <Sider style={siderStyle}>
+        <List
+          size="small"
+          bordered
+          dataSource={data}
+          renderItem={(item) => <List.Item>{item}</List.Item>}
+        />
+      </Sider>
+      <Layout>
+        <Header style={headerStyle}>Header</Header>
+        <Content style={contentStyle}>
+          <Card>
+            <Outlet />
+          </Card>
+        </Content>
+        <Footer style={footerStyle}>Footer</Footer>
+      </Layout>
+    </Layout>
   );
 }
